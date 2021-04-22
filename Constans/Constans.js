816bb9986e6383c07fb657a1c46db710986f3801
szymel2020 <szymon.chala@gmail.com>
2021-04-22 22:15:55 +0200

    constans

---
+++ Constans/Constans.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+
+	const pi = 3.142;
+	
+
+function calcArea(r) {
+	const pi = 10;
+	console.log("The area is : " + pi * r * r);
+}
+	
+	console.log(pi);
+	calcArea(5);
+}
+
+// Zmienna "const" w zakresie globalnym nie zmienia swojej wartości, ale wewnątrz funkcji możemy jej nadać inną wafrtość i zadziała.
